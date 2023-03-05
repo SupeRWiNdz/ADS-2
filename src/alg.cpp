0b4419aefe8275ac1e936ecc74be9a35a6d0b6b5
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-03-05 17:30:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,30 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-
+    if (n == 0) return 1;
+    double result = value;
+    for (uint16_t i = 1; i < n; i++)
+        result *= value;
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+    return 0;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return 0;
 }
 
 double expn(double x, uint16_t count) {
-
+    return 0;
 }
 
 double sinn(double x, uint16_t count) {
-
+    return 0;
 }
 
 double cosn(double x, uint16_t count) {
-
+    return 0;
 }
