f31a871d2cf5da9524f2c1361520c636505db32d
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-03-05 18:56:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,9 +11,10 @@ return result;
 }
 
 uint64_t fact(uint16_t n) {
-if (n == 0) return (1);
-else
-return (n * fact(n - 1));
+uint64_t result = 1;
+for (uint16_t i = n; i > 0; i--) {
+result *= i;}
+return result;
 }
 
 double calcItem(double x, uint16_t n) {
@@ -30,13 +31,13 @@ return result;
 double sinn(double x, uint16_t count) {
 double result = 0;
 for (uint16_t i = 0; i <= count; i++)
-result += (calcItem(x, 2 * i + 1) * pown(-1, i));
+result += (calcItem(x, 2 * i - 1) * pown(-1, i - 1));
 return result;
 }
 
 double cosn(double x, uint16_t count) {
 double result = 0;
 for (uint16_t i = 0; i <= count; i++)
-result += (calcItem(x, 2 * i) * pown(-1, i));
+result += (calcItem(x, 2 * i - 2) * pown(-1, i - 1));
 return result;
 }
